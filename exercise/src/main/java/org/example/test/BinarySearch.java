df994ab8409c4cfcebb463bbf7253a141182c4d2
rpf <1103907900@qq.com>
2023-04-22 23:07:31 +0800

    code optimize

--- exercise/src/main/java/org/example/test/BinarySearch.java
+++ exercise/src/main/java/org/example/test/BinarySearch.java
@@ -29,4 +29,29 @@ public class BinarySearch {
         }
         return -1;
     }
+
+    /**
+     * 二分查找优化(左右查找平衡)
+     *
+     * @param a      有序数组
+     * @param target 待查找目标值
+     * @return 找到返回索引，否则返回-1
+     */
+    public static int binarySearch(int[] a, int target) {
+        int i = 0;
+        int j = a.length;
+        while (1 < j - i) {
+            int m = (i + j) >>> 1;
+            if (target < a[m]) {
+                j = m;
+            } else {
+                i = m;
+            }
+        }
+        if (target == a[i]) {
+            return i;
+        } else {
+            return -1;
+        }
+    }
 }
--- exercise/src/test/java/org/example/test/BinarySearchTest.java
+++ exercise/src/test/java/org/example/test/BinarySearchTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 
+import static org.example.test.BinarySearch.binarySearch;
 import static org.example.test.BinarySearch.binarySearchBasic;
 
 /**
@@ -25,5 +26,19 @@ public class BinarySearchTest {
         Assertions.assertEquals(5, binarySearchBasic(a, 26));
         Assertions.assertEquals(6, binarySearchBasic(a, 40));
         Assertions.assertEquals(-1, binarySearchBasic(a, 4));
+    }
+
+    @Test
+    @DisplayName(value = "二分查找数据")
+    public void binarySearch_CodeOptimizeTest() {
+        int[] a = new int[]{1, 3, 5, 6, 24, 26, 40};
+        Assertions.assertEquals(0, binarySearch(a, 1));
+        Assertions.assertEquals(1, binarySearch(a, 3));
+        Assertions.assertEquals(2, binarySearch(a, 5));
+        Assertions.assertEquals(3, binarySearch(a, 6));
+        Assertions.assertEquals(4, binarySearch(a, 24));
+        Assertions.assertEquals(5, binarySearch(a, 26));
+        Assertions.assertEquals(6, binarySearch(a, 40));
+        Assertions.assertEquals(-1, binarySearch(a, 4));
     }
 }
